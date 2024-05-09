9953829d58d460fded2ee2170ec9f8c6bd438f97
kwabenadarkwa <darkwak@live.com>
2024-05-09 20:09:47 +0100

    :sparkles: include auto imports

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -11,12 +11,12 @@ return {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
-        -- null_ls.builtins.completions.gci,
+        -- null_ls.builtins.diagnostics.gci,
         -- null_ls.builtins.formatting.golangci-lint,
         --null_ls.builtins.diagnostics.cpplint,
-        --  null_ls.builtins.completion.spell,
         --  This is the syntax for adding linters 
         require("none-ls.formatting.eslint_d"),  -- requires none-ls-extras.nvim
+        -- require("none-ls.diagnostics.gci"),  -- requires none-ls-extras.nvim
         require("none-ls.diagnostics.cpplint"),   -- requires none-ls-extras.nvim
       },
     })
