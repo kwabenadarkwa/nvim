2113c23c827b5752d1253225c0d55cbac103482b
kwabenadarkwa <darkwak@live.com>
2024-05-19 19:35:31 +0100

    :fire: add lazy for merge conflict fixing

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -1,14 +1,34 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      -- this it not i
-      require("gitsigns").setup()
-      vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview<CR>")
-      vim.keymap.set("n", "<leader>tb", "<cmd>Gitsigns toggle_current_line_blame<CR>")
-    end,
-  },
-  {
-    "tpope/vim-fugitive",
-  },
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			-- this it not i
+			require("gitsigns").setup()
+			vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview<CR>")
+			vim.keymap.set("n", "<leader>tb", "<cmd>Gitsigns toggle_current_line_blame<CR>")
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		-- nvim v0.8.0
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
 }
