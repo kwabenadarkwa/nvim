615fea9dff02cd5769679310a2b2281c1965511e
kwabenadarkwa <darkwak@live.com>
2024-06-05 09:07:40 +0000

    :recycle: change pylyzer to pyright

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -26,7 +26,7 @@ return {
     }
 
     dashboard.section.buttons.val = {
-      dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+      dashboard.button("f", "   Find file", ":Telescope find_files <CR>"),
       dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
       dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
       dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,63 +1,63 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"clangd",
-					"unocss",
-					"tsserver",
-					"pylyzer",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilites = capabilities,
-				settings = {
-					lua_ls = { completeUnimported = true },
-				},
-			})
-			lspconfig.clangd.setup({
-				capabilites = capabilities,
-				settings = {
-					clangd = { completeUnimported = true },
-				},
-			})
-			lspconfig.unocss.setup({
-				capabilites = capabilities,
-				settings = {
-					unocss = { completeUnimported = true },
-				},
-			})
-			lspconfig.tsserver.setup({
-				capabilites = capabilities,
-				settings = {
-					tsserver = { completeUnimported = true },
-				},
-			})
-			lspconfig.pylyzer.setup({
-				capabilites = capabilities,
-				settings = {
-					pylyzer = { completeUnimported = true },
-				},
-			})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, {})
-		end,
-	},
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "clangd",
+          "unocss",
+          "tsserver",
+          "pyright"
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilites = capabilities,
+        settings = {
+          lua_ls = { completeUnimported = true },
+        },
+      })
+      lspconfig.clangd.setup({
+        capabilites = capabilities,
+        settings = {
+          clangd = { completeUnimported = true },
+        },
+      })
+      lspconfig.unocss.setup({
+        capabilites = capabilities,
+        settings = {
+          unocss = { completeUnimported = true },
+        },
+      })
+      lspconfig.tsserver.setup({
+        capabilites = capabilities,
+        settings = {
+          tsserver = { completeUnimported = true },
+        },
+      })
+      lspconfig.pyright.setup({
+        capabilites = capabilities,
+        settings = {
+          pyright = { completeUnimported = true },
+        },
+      })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, {})
+    end,
+  },
 }
