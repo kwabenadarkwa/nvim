f29caab6b6803eb3b4f979cd01909b88a70d06d3
kwabenadarkwa <darkwak@live.com>
2024-07-10 09:11:03 +0000

    remove nvim notify from noice dependencies

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -12,8 +12,8 @@ return {
 		-- OPTIONAL:
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
-		"nvim-notify",
+		-- "rcarriga/nvim-notify",
+		-- "nvim-notify",
 	},
 	config = function()
 		local config = require("noice")
