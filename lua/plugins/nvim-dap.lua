5b8b12bdb6abe5a2f6f5bdaeffbc236e0bf72281
kwabenadarkwa <darkwak@live.com>
2024-11-11 16:50:58 +0000

    change things

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,4 +1,24 @@
 --this package is a debug adapter protocol client for nvim(I don't understnad this yet)
-return{
-  "mfussenegger/nvim-dap"
+return {
+  "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require("dap")
+
+    dap.adapters.coreclr = {
+      type = "executable",
+      command = "/usr/local/bin/netcoredbg/netcoredbg",
+      args = { "--interpreter=vscode" },
+    }
+
+    dap.configurations.cs = {
+      {
+        type = "coreclr",
+        name = "launch - netcoredbg",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+        end,
+      },
+    }
+  end,
 }
